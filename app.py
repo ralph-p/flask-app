d83131eaf24434706710f3128b982fd418f72402
ralph-p <mr.pereira91@gmail.com>
2023-03-14 21:22:46 -0400

    added post call

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from store import notes
+from flask import Flask, jsonify, request, Response, json
+from store import notes, add_game_to_DB, find_all_notes
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,15 +10,30 @@ def hello_world():
 @app.route('/notes')
 def all_notes():
     return jsonify({
-        'notes': notes,
+        'notes': find_all_notes(),
     })
 
 @app.route('/notes/<note_id>')
 def find_note_by_id(note_id):
     for note in notes:
-        if note["id"] == int(note_id):
+        if note["id"] == note_id:
             return jsonify({
                 "note": note,
             })
+
+@app.route('/notes/add', methods=['POST'])
+def add_game():
+    print(json.loads(request.data))
+    data = request.get_json()
+    try:
+        note = data['note']
+        time = data['time']
+        if note and time:
+            data = add_game_to_DB(note, int(time))
+            return jsonify(data), 201
+    except Exception as e:
+        print(e)
+        return Response('''{"message": "Bad Request"}''', status=400, mimetype='application/json')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
--- store.py
+++ store.py
@@ -15,4 +15,17 @@ def find_all_notes():
     json_data = json.loads(response_data)['data']
     return json_data
 
+def add_game_to_DB(note, time) -> dict:
+
+    game = {
+        "note": note,
+        "time": time
+    }
+    data = supabase.table("notes").insert(game).execute()
+    # Equivalent to the SQL Insert
+
+    response_data = data.json()
+    json_data = json.loads(response_data)['data']
+    return json_data
+
 notes = find_all_notes()
